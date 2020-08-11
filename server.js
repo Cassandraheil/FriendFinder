9b8ff39b3d22661487e655863bff7deb6cb26086
Cassandra Heil <Cassandraheil19@gmail.com>
2020-08-11 18:11:48 -0400

    styling page

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require("express");
+var path = require('path')
 
 var app = express();
 
 var PORT = process.env.PORT || 3000;
+
+app.use(express.static("/public"));
+app.use('/css',express.static(path.join(__dirname, 'public/css')));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,7 +14,7 @@ app.use(express.json());
 require("./app/routing/htmlRoutes")(app);
 require("./app/routing/apiRoutes")(app);
 
-app.use(express.static("/public"));
+
 
 
 app.listen(PORT, function() {
